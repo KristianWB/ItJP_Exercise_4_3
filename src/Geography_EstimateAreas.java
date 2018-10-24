71c4558ac3c25aab870ab3142fce07b45defddfc
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-24 23:29:37 +0200

    Initial commit

---
+++ src/Geography_EstimateAreas.java
@@ -0,0 +1,36 @@
+public class Geography_EstimateAreas {
+
+    /* I will solve this exercise in 4 steps. First i will write in the four coordinates as stated in the book, Then
+     * i will choose 2 triangles and calculate the distances between the triangle coordinates from the formula given in
+      * Exercise 4.2. Third i calculate the area from the formula given in exercise 2.19 and fourth i print out the
+       * results */
+    public static void main(String[] args)   {
+
+        // This is the first part where i type in the coordinates
+        double charlotteX = 35.2270869; // X coordinate from the city Charlotte
+        double charlotteY = -80.8431267; // Y coordinate from the city Charlotte
+
+        double savannahX = 32.0835407; // X coordinate from the city Savannah
+        double savannahY = -81.0998342; // Y coordinate from the city Savannah
+
+        double orlandoX = 28.5383355; // X coordinate from the city Orlando
+        double orlandoY = -81.3792365; // Y coordinate from the city Orlando
+
+        double atlantaX = 33.7489954; // X coordinate from the city Atlanta
+        double atlantaY = -84.3879824; // Y coordinate from the city Atlanta
+
+        /* Now the second part starts and i must choose two triangles before i continue.
+         I choose Charlotte-Savannah-Atlanta and Atlanta-Savannah-Orlando. Then i must calculate the distances
+          between the coordinates using the formula from exercise 4.2*/
+
+            // First i convert all the data in degrees into radians
+
+        double charlotteRadX = Math.toRadians(35.2270869);
+        double yRad1 = Math.toRadians(y1);
+        double xRad2 = Math.toRadians(x2);
+        double yRad2 = Math.toRadians(y2);
+
+
+
+    }
+}
